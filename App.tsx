d84f2a4abc04e12b15f1542c40055476a619ab02
unknown <opokudavid446@gmail.com>
2020-08-12 14:35:26 +0100

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,23 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Text } from "react-native";
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <SafeAreaProvider>
+      <Text>hfd</Text>
+    </SafeAreaProvider>
+  );
 }
